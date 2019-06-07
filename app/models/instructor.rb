8b9bd86b81d02ba7c370d3a71fa8f945cce3e3e3
Spencer Lindemuth <lindemut@gmail.com>
2019-06-07 16:04:41 -0700

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,41 @@
 class Instructor
 
+    attr_reader :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+
+    def pass_student(student, test_name)
+        BoatingTest.all.each do |x|
+            if x.test_name == test_name
+                if x.student == student
+                    x.test_status = "passed"
+                    return x
+                end
+            else
+                BoatingTest.new(student, test_name, "passed", self)
+            end
+        end
+    end
+
+    def fail_student(student, test_name)
+        BoatingTest.all.each do |x|
+            if x.test_name == test_name
+                if x.student == student
+                    x.test_status = "failed"
+                    return x
+                end
+            else
+                BoatingTest.new(student, test_name, "failed", self)
+            end
+        end
+    end
+
+    def self.all
+        @@all
+    end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,40 @@
 class Student
 
+    attr_reader :first_name
+
+    @@all = []
+    def initialize(first_name)
+        @first_name = first_name
+        @@all << self
+    end
+
+    def add_boating_test(test_name, test_status, instructor)
+        BoatingTest.new(self, test_name, test_status, instructor)
+    end
+
+    def self.find_student(first_name)
+        Student.all.find do |student|
+            student.first_name == first_name
+        end
+    end
+
+    def grade_percentage
+        array = BoatingTest.all.map do |x|
+            if x.student == self
+                x.test_status
+            end
+        end
+        trim = array.select{ |x| x }
+        dec = trim.count("passed").to_f / trim.length.to_f
+        dec * 100
+        #binding.pry
+
+
+    end
+
+
+    def self.all
+        @@all
+    end
+
 end
